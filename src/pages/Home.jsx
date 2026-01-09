dc4dc715668b9a6688364e18935fe41e4c03d81c
Matija Miletić <matoma002@gmail.com>
2026-01-09 20:05:16 +0100

    Created Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+      <section className="hero">
+        <h1>Movie search engine</h1>
+        <h5>Powered by OMDB</h5>
+      </section>
+    </>
+  );
+}
+export default Home;
